f6c053a1b80ea48842af5fd3f632c72c5938aae0
felex <engineerfelex@gmail.com>
2020-11-19 16:38:42 +0300

    create task clas with consturoctor and maps

---
+++ lib/models/task_model.dart
@@ -0,0 +1,29 @@
+class Task {
+  int id;
+  String title;
+  DateTime date;
+  String priority;
+  int status;
+  Task.withId({this.id, this.title, this.status, this.date, this.priority});
+  Task({this.title, this.status, this.date, this.priority});
+
+  Map<String, dynamic> toMap() {
+    final map = Map<String, dynamic>();
+    map['id'] = id;
+    map['title'] = title;
+    map['date'] = date;
+    map['priority'] = priority;
+    map['status'] = status;
+    return map;
+  }
+
+  factory Task.fromMap(Map<String, dynamic> map) {
+    return Task.withId(
+      id: map['id'],
+      title: map['title'],
+      date: DateTime.parse(map['date']),
+      status: map['status'],
+      priority: map['priority'],
+    );
+  }
+}
